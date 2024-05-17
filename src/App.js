842db875036283e6420d762421b1500accbf9d57
Thomas McConnell <thomasmcconnell24@icloud.com>
2024-05-17 05:56:35 -0700

    EVERYTHING REDESIGN - BOWLS STILL OVERHANGING

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './components/HomePage';
-import FoodPage from './components/FoodPage';
-import WaterPage from './components/WaterPage';
+import MainPage from './components/MainPage';
+import './App.css';
 
-const App = () => {
+function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/food" element={<FoodPage />} />
-        <Route path="/water" element={<WaterPage />} />
-      </Routes>
-    </Router>
+    <div className="App">
+      <MainPage />
+    </div>
   );
-};
+}
 
 export default App;
